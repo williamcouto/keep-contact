3562554e4de39520569c652b7412391fbe6c1d26
williamcouto <willplacido40@gmail.com>
2025-04-16 20:04:45 -0300

    Feat: Inserindo os cards de email e telefone Corrigindo indentação e modficando a lógica do botão de exclusão do contato

--- script.js
+++ script.js
@@ -2,58 +2,58 @@ let bttnAdd = document.querySelector('.btn')
 let form = document.getElementById('formContact')
 let contact = []
 
-function contactHandler(bttnAdd, form){
+function contactHandler(bttnAdd, form) {
     form.addEventListener('submit', (Event) => Event.preventDefault())
     bttnAdd.addEventListener('click', () => {
         let nome = document.getElementById('contactName').value
         let email = document.getElementById('emailContact').value
         let tel = document.getElementById('telContact').value
-        
-        if(checkFields(nome, email, tel)){  
+
+        if (checkFields(nome, email, tel)) {
             cardErrorNotification()
             return;
-        } 
-    
+        }
+
         const validTel = validateInput(tel)
         const validEmail = validateInputEmail(email)
 
-        if(!validTel){
+        if (!validTel) {
             window.alert('O número de celular é inválido')
-                return;
+            return;
         }
-        if(!validEmail){
+        if (!validEmail) {
             window.alert('O email inserido é inválido')
-                return;
+            return;
         }
         cardSuccessInsert()
         setTimeout(removeCards, 1000)
         addContact(nome, email, tel)
-    })   
+    })
 }
 // Validando campos
-function validateInput(tel){
+function validateInput(tel) {
     const regexTel = /^\([1-9]{2}\)\s9[0-9]{4}\-[0-9]{4}$/
     return regexTel.test(tel)
 }
 
 //Validando campo de email
-function validateInputEmail(email){
+function validateInputEmail(email) {
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     return regexEmail.test(email)
 }
 
 // Verificando se os campos estão vazios
-function checkFields(){
+function checkFields() {
     const fields = Array.from(arguments)
     return fields.some(field => field === "")
 }
 
 //Criando cards de notificação
-function cardErrorNotification(){
+function cardErrorNotification() {
     let cardError = document.querySelector('.alert-danger')
-    
+
     //Garante que apenas uma seja exibida por vez.
-    if(cardError){
+    if (cardError) {
         cardError.remove()
     }
     let card = document.createElement('span')
@@ -72,7 +72,7 @@ function cardErrorNotification(){
     card.style.justifyContent = 'space-between'
 
     //Fecha o card quando clica no botão de fechar(X)
-    btnCloseCard.addEventListener('click', () =>{
+    btnCloseCard.addEventListener('click', () => {
         card.remove()
     })
 }
@@ -83,31 +83,28 @@ function cardSuccessInsert() {
     let card = document.createElement('span')
     let textSuccess = document.createTextNode('O contato foi adicionado!')
     card.setAttribute('class', 'alert alert-success')
-    
+
     card.appendChild(textSuccess)
     form.appendChild(card)
 }
 
-const removeCards = function(){
+const removeCards = function () {
     let cards = document.querySelectorAll('span')
     cards.forEach(
         card => card.remove()
     )
 }
 
-function addContact(nome, email, tel){
-    contact.push({nome, email, tel})
+function addContact(nome, email, tel) {
+    contact.push({ nome, email, tel })
     saveContactStorage(contact)
     createRow(nome, email, tel)
     limparCampos()
 }
 
-function createRow(nome, email, tel){
-    /**Inserindo cards */
-  
+function createRow(nome, email, tel) {
     const cardSect = document.querySelector('.card-container')
     let textElem = ''
-
     let cardUser = document.createElement('div')
     cardUser.setAttribute('class', 'card')
 
@@ -116,65 +113,43 @@ function createRow(nome, email, tel){
 
     cardSect.appendChild(cardUser)//Insere 
     cardSect.appendChild(cardBody)
-
     cardUser.appendChild(cardBody)
 
     //Criando labels do card
-    let nameLabel = document.createElement('h5')
-    nameLabel.setAttribute('class', 'card-title')
-
-
+    let nameContactUser = document.createElement('h5')
+    nameContactUser.setAttribute('class', 'card-title')
     textElem = document.createTextNode(nome)
-    nameLabel.appendChild(textElem)
-    cardBody.appendChild(nameLabel)
+    nameContactUser.appendChild(textElem)
+    cardBody.appendChild(nameContactUser)
 
-    let emailLabel = document.createElement('p')
-    emailLabel.setAttribute('class', 'card-text')
-    
-
+    let emailContactUser = document.createElement('p')
+    emailContactUser.setAttribute('class', 'card-text')
     textElem = document.createTextNode(email)
-    emailLabel.appendChild(textElem)
-    cardBody.appendChild(emailLabel)
-    // const tbAdd = document.getElementById('tb-contact')
-    // let newRow = document.createElement('tr')
-    // let textElem = ''
+    emailContactUser.appendChild(textElem)
+    cardBody.appendChild(emailContactUser)
 
-    // let tdName = document.createElement('td')
-    // textElem = document.createTextNode(nome)
-    // tdName.appendChild(textElem)
-    // newRow.appendChild(tdName)
-    // tbAdd.appendChild(newRow)
+    // adicionando o telefone
+    let telContactUser = document.createElement('p')
+    telContactUser.setAttribute('class', 'card-text')
+    textElem = document.createTextNode(tel)
+    telContactUser.appendChild(textElem)
+    cardBody.appendChild(telContactUser)
 
-    // let tdEmail = document.createElement('td')
-    // textElem = document.createTextNode(email)
-    // tdEmail.appendChild(textElem)
-    // newRow.appendChild(tdEmail)
-    // tbAdd.appendChild(newRow)
+    limparCampos()
 
-    // let tdCel = document.createElement('td')
-    // textElem = document.createTextNode(tel)
-    // tdCel.appendChild(textElem)
-    // newRow.appendChild(tdCel)
-    // tbAdd.appendChild(newRow)
+    // Criando botão de exclusão
+    let btnRemove = document.createElement('button')
+    btnRemove.classList.add('btn', 'btn-danger')
+    btnRemove.innerHTML = '<i class="bi bi-trash3-fill"></i>'
+    cardBody.appendChild(btnRemove)
 
-    // limparCampos()
-
-    // // Criando botão de exclusão
-    // let tdRemove = document.createElement('td')
-    // let btnRemove = document.createElement('button')
-    // btnRemove.classList.add('btn', 'btn-danger')
-    // btnRemove.innerHTML = '<i class="bi bi-trash3-fill"></i>'
-    // newRow.appendChild(tdRemove)
-    // tdRemove.appendChild(btnRemove)
-
-    // btnRemove.addEventListener('click', (Event) => {
-    //     const rowRemoved = Event.target.closest('tr')
-    //     rowRemoved.remove()
-    //     removeContato(nome,email,tel)
-    // })
+    btnRemove.addEventListener('click', () => {
+        cardBody.remove()
+        removeContato(nome, email, tel)
+    })
 }
 //Limpando valores
-function limparCampos(){
+function limparCampos() {
     document.getElementById('contactName').value = ''
     document.getElementById('emailContact').value = ''
     document.getElementById('telContact').value = ''
@@ -188,14 +163,14 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTable()
 })
 
-function saveContactStorage(contact){
+function saveContactStorage(contact) {
     localStorage.setItem('contact', JSON.stringify(contact))
 }
 function loadStorage() {
     let contact = localStorage.getItem('contact')
     return contact ? JSON.parse(contact) : []
 }
-function updateTable(){
+function updateTable() {
     let contact = loadStorage()
     let tbodyStorage = document.querySelector('tbody')
     tbodyStorage.innerHTML = ""
@@ -203,7 +178,7 @@ function updateTable(){
         createRow(contato.nome, contato.email, contato.tel)
     })
 }
-function removeContato(nome, email, tel){
+function removeContato(nome, email, tel) {
     contact = contact.filter(contato => !(contato.nome === nome && contato.email === email && contato.tel === tel))
     saveContactStorage(contact)
 }
